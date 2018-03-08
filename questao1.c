1194caeb37fd5229304f6aadb3abbd829888d4bf
João Carlos Cortez Gomes <joaocarlos_oficial@hotmail.com>
2018-03-08 10:47:11 -0300

    Questao 01

---
+++ questao1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n1;
+    int n2;
+    int aux;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &n1);
+
+    printf("Digite o último número: ");
+    scanf("%d", &n2);
+
+    for(aux=n1; aux<=n2; aux++){
+        if((aux % 2) == 0 & (aux % 3) == 0){
+            printf("%d \n", aux);
+        }
+    }
+    return 0;
+}
