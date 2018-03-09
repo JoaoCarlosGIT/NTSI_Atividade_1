92b3137a00880aada0278fdb7f42c5eecb7236db
João Carlos Cortez Gomes <joaocarlos_oficial@hotmail.com>
2018-03-09 14:08:31 -0300

    Questão 03

--- questao3.c
+++ questao3.c
@@ -2,7 +2,9 @@
 
 int main()
 {
-    int a, b, c, aux;
+    int a; 
+    int b; 
+    int c;
 
     printf("Digite três numeros naturais: \n");
     printf("Valor A: ");
@@ -12,20 +14,11 @@ int main()
     printf("Valor C: ");
     scanf("%d", &c);
 
-    if (b > a)
-    {
-        aux = a;
-        a = b;
-        b = aux;
-    }
-    if (c > a)
-    {
-        aux = a;
-        a = c;
-        c = aux;
-    }
-
     if (a * a == b * b + c * c)
+        printf("O triangulo é retangulo! \n");
+    if else (b * b == a * a + c * c)
+        printf("O triangulo é retangulo! \n");
+    if else (c * c == a * a + b * b)
         printf("O triangulo é retangulo! \n");
     else
         printf("O triangulo não é retangulo! \n");
